0f04f58f2064810962fd574769420a4b7a99024f
topilski <atopilski@gmail.com>
2015-07-27 16:39:30 +0300

    Code style

--- FindSasl2.cmake
+++ FindSasl2.cmake
@@ -13,16 +13,16 @@
 #
 
 FIND_PATH(SASL2_INCLUDE_DIR NAMES sasl/sasl.h
-                             PATHS /include /usr/include /usr/local/include /usr/share/include /opt/include 
-                             DOC "Try and find the header file sasl/sasl.h")
+            PATHS /include /usr/include /usr/local/include /usr/share/include /opt/include
+            DOC "Try and find the header file sasl/sasl.h")
 
 FIND_LIBRARY(SASL2_LIBRARIES NAMES sasl2
-                           PATHS /usr/lib /lib /usr/local/lib /usr/share/lib /opt/lib /opt/share/lib /var/lib
-                           DOC "Try and find libsasl2")
+            PATHS /usr/lib /lib /usr/local/lib /usr/share/lib /opt/lib /opt/share/lib /var/lib
+            DOC "Try and find libsasl2")
 
-mark_as_advanced(SASL2_LIBRARIES SASL2_INCLUDE_DIR)
+MARK_AS_ADVANCED(SASL2_LIBRARIES SASL2_INCLUDE_DIR)
 
 # handle the QUIETLY and REQUIRED arguments and set SASL2_FOUND to TRUE if
 # all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
+INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SASL2 REQUIRED_VARS SASL2_LIBRARIES SASL2_INCLUDE_DIR)
