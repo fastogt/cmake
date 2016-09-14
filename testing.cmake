cb749b4e0c636ade7d8f7128665f1d0d47a46191
topilski <atopilski@gmail.com>
2016-09-14 12:13:13 -0400

    Macro for testing

--- testing.cmake
+++ testing.cmake
@@ -1,4 +1,8 @@
 MACRO(SETUP_TESTING)
-        enable_testing()
-        include(CTest)
+  enable_testing()
+  include(CTest)
 ENDMACRO(SETUP_TESTING)
+
+MACRO(ADD_TEST_TARGET target)
+  ADD_TEST(NAME ${target} COMMAND $<TARGET_FILE:${target}>)
+ENDMACRO(ADD_TEST_TARGET)
