7979e8a9ed4585a7cc5ca9dc469d58d70fcc3677
atopilski <atopilski@gmail.com>
2021-07-15 02:49:30 -0400

    Find LZ4

--- FindSnappy.cmake
+++ FindSnappy.cmake
@@ -1,37 +1,29 @@
-# - Find Snappy 
+# - Find Snappy
 # Find the snappy compression library and includes
 #
-#  SNAPPY_INCLUDE_DIRS - where to find snappy.h, etc.
-#  SNAPPY_LIBRARIES   - List of libraries when using snappy.
-#  SNAPPY_FOUND       - True if snappy found.
+# Snappy_INCLUDE_DIRS - where to find snappy.h, etc.
+# Snappy_LIBRARIES - List of libraries when using snappy.
+# Snappy_FOUND - True if snappy found.
 
-IF(SNAPPY_USE_STATIC)
-  MESSAGE(STATUS "SNAPPY_USE_STATIC: ON")
-ELSE()
-  MESSAGE(STATUS "SNAPPY_USE_STATIC: OFF")
-ENDIF(SNAPPY_USE_STATIC)
+find_path(Snappy_INCLUDE_DIRS
+  NAMES snappy.h
+  HINTS ${snappy_ROOT_DIR}/include)
 
-FIND_PATH(SNAPPY_INCLUDE_DIRS snappy.h PATHS
-  /usr/include
-  /opt/local/include
-  /usr/local/include
-)
-IF(SNAPPY_USE_STATIC)
-  SET(SNAPPY_NAMES ${SNAPPY_NAMES} libsnappy.a)
-ELSE()
-  SET(SNAPPY_NAMES ${SNAPPY_NAMES} snappy)
-ENDIF()
+find_library(Snappy_LIBRARIES
+  NAMES snappy
+  HINTS ${snappy_ROOT_DIR}/lib)
 
-FIND_LIBRARY(SNAPPY_LIBRARIES NAMES ${SNAPPY_NAMES} PATHS
-  /usr/local/lib
-  /opt/local/lib
-  /usr/lib
-)
-
-# handle the QUIETLY and REQUIRED arguments and set SNAPPY_FOUND to TRUE if
-# all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Snappy
-                                  REQUIRED_VARS SNAPPY_LIBRARIES SNAPPY_INCLUDE_DIRS
-                                  VERSION_VAR SNAPPY_VERSION_STRING)
-MARK_AS_ADVANCED(SNAPPY_INCLUDE_DIRS)
+find_package_handle_standard_args(Snappy DEFAULT_MSG Snappy_LIBRARIES Snappy_INCLUDE_DIRS)
+
+mark_as_advanced(
+  Snappy_LIBRARIES
+  Snappy_INCLUDE_DIRS)
+
+if(Snappy_FOUND AND NOT (TARGET Snappy::snappy))
+  add_library (Snappy::snappy UNKNOWN IMPORTED)
+  set_target_properties(Snappy::snappy
+    PROPERTIES
+      IMPORTED_LOCATION ${Snappy_LIBRARIES}
+      INTERFACE_INCLUDE_DIRECTORIES ${Snappy_INCLUDE_DIRS})
+endif()
