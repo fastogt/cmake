eb5417ea3bb490b3820ea0496d2f2f9db2ac027c
topilski <atopilski@gmail.com>
2022-06-14 08:02:59 -0600

    Init mod

--- go/golang.cmake
+++ go/golang.cmake
@@ -111,13 +111,16 @@ SET(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_DIRECTORY})
 
 function(add_go_executable NAME)
   file(GLOB GO_SOURCE RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
+  add_custom_target(init ALL
+    COMMAND env GOPATH=${GOPATH} ${CMAKE_GO_COMPILER} mod tidy
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
   add_custom_command(OUTPUT ${OUTPUT_DIR}/.timestamp 
     COMMAND env GOPATH=${GOPATH} ${CMAKE_GO_COMPILER} build
     -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
     ${CMAKE_GO_FLAGS} ${GO_SOURCE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
 
-  add_custom_target(${NAME} ALL DEPENDS ${OUTPUT_DIR}/.timestamp ${ARGN})
+  add_custom_target(${NAME} ALL DEPENDS init ${OUTPUT_DIR}/.timestamp ${ARGN})
 endfunction(add_go_executable)
 
 INCLUDE(../projecthelper)
