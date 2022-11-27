b13774ddc003e7a5d6a0116b594a9dbe7313851a
topilski <atopilski@gmail.com>
2022-11-27 02:59:52 -0500

    Hot fix

--- FindDeepStream.cmake
+++ FindDeepStream.cmake
@@ -32,7 +32,7 @@ set(DeepStream_LIBRARY_NAMES
 	libnvbufsurftransform.so
 	libnvds_amqp_proto.so
 	libnvds_inferutils.so
-	libnvds_nvdcf.so
+#	libnvds_nvdcf.so
 	libnvds_azure_edge_proto.so
 	libnvds_kafka_proto.so
 	libnvds_nvtxhelper.so
@@ -42,10 +42,10 @@ set(DeepStream_LIBRARY_NAMES
 	libnvds_meta.so
 	libnvds_osd.so
 	libnvdsgst_helper.so
-	libnvds_mot_iou.so
+#	libnvds_mot_iou.so
 	libnvds_utils.so
 	libnvdsgst_meta.so
-	libnvds_mot_klt.so
+#	libnvds_mot_klt.so
 	)
 
 if (DeepStream_FOUND)
